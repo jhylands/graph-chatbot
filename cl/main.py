fa53f9a23b5e1f397ce5afaea51524659bb39e8d
jhylands <hylands.james@gmail.com>
2019-12-12 00:37:27 +0000

    Step up, some context

--- cl/main.py
+++ cl/main.py
@@ -1,13 +1,25 @@
 # Starting off just trying to build a simple repl
 
-def handle(response):
-    return "You said %s"%response
+def anything(context, response):
+    if response=="add handle":
+        context = "ADD"
+        return context, "Please enter the pattern to match:"
+    else:
+        return context , "You said %s"%response
+        
+def add(context, response):
+    return "ANYTHING", "Given the command what should I respond?"
+    
+handlers = {"ANYTHING": anything, "ADD": add}
+def handle(context, response):
+    return handlers[context](context, response)
 
 def main():
     statement = "Welcome to LIRA"
+    context = "ANYTHING"
     while True:
         print(statement)
         response = input()
-        statement = handle(response)
+        context, statement = handle(context, response)
 
 main()
