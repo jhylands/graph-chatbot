8580a1b6bc1a1ae2b793978da90b5b56d9787a94
jhylands <hylands.james@gmail.com>
2019-12-12 00:53:44 +0000

    Moving to a graph context

--- cl/main.py
+++ cl/main.py
@@ -1,4 +1,21 @@
 # Starting off just trying to build a simple repl
+import re
+
+class Node:
+    def __init__(self, name=""):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+class Edge:
+    start = None
+    style = None
+    end = None
+    def __init__(self, start, style, end):
+        self.start = start
+        self.style = style
+        self.end = end
 
 def anything(context, response):
     if response=="add handle":
@@ -12,14 +29,28 @@ def add(context, response):
     
 handlers = {"ANYTHING": anything, "ADD": add}
 def handle(context, response):
+    
     return handlers[context](context, response)
+
+def init_context():
+    root = Node("root_node")
+    return root
+
+# I guess this will eventually take context as well
+def match_response(response_map, response):
+    matches = [function for regex, function in response_map if re.match(regex, response)]
+    if len(matches)>1:
+        print("More than one regex matched!")
+    else:
+        return matches[0]
 
 def main():
     statement = "Welcome to LIRA"
-    context = "ANYTHING"
+    context = init_context()
+    response_map = [(".*", anything)]
     while True:
         print(statement)
         response = input()
-        context, statement = handle(context, response)
+        context, statement = match_response(response_map, response)(context, response)
 
 main()
